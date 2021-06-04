484598aa35a88424d104cc0d6889031cf43ceda9
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-06-04 11:00:42 -0500

    Home reverted to home page.

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -7,7 +7,7 @@ import twoPlayer from "../app/Screens/twoPlayer"
 // Used for "Stack Navigation"
 const screens = {
     Home : {
-        screen : singleLifeTotal,
+        screen : homePage,
     },
     Single: {
         screen : singleLifeTotal,
